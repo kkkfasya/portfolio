062d2d1d6ab09788ad1f3550ee0b234c710a86c3
Kamal Fasya <kamalfasya01@gmail.com>
2026-07-26 04:19:04 +0700

    A rabbit falls away from me, I guess I'll crawl

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+
+  fonts: [
+    {
+      provider: fontProviders.google(),
+      name: "EB Garamond",
+      cssVariable: "--font-eb",
+    },
+  ],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
